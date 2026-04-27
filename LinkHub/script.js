2438f24ec0d8711d2478395e5e9227fc6a37d969
m9204730-create <m9204730@gmail.com>
2026-04-27 19:27:30 +0300

    Ultra modern redesign

--- LinkHub/script.js
+++ LinkHub/script.js
@@ -1,8 +1,10 @@
 const btn = document.getElementById('themeToggle');
 
 btn.addEventListener('click', () => {
+    
     document.body.classList.toggle('dark');
 
+    
     if (document.body.classList.contains('dark')) {
         btn.textContent = '☀️ Світла тема';
     } else {
